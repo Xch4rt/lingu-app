a6c64717ab92f300a6f198ed201366b5bc072702
Xch4rt <pab203.guti@gmail.com>
2023-10-29 17:22:10 -0600

    users: guards a main config

--- apps/users/src/users.controller.ts
+++ apps/users/src/users.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { ApiTags } from '@nestjs/swagger';
 import { RegisterUserDto } from './dto/register-user.users.dto';
 import { PaginationDto } from 'libs/common/dto/pagination/pagination.dto';
+import { FirebaseAuthGuard } from 'libs/guards/auth.guard';
 
 @ApiTags('Users')
 @Controller('users')
@@ -15,21 +16,25 @@ export class UsersController {
   }
 
   @Get()
+  @UseGuards(FirebaseAuthGuard)
   async findAll(@Param() paginationDto : PaginationDto) {
     return this.usersService.findAll(paginationDto);
   }
 
   @Get('profile/:id')
+  @UseGuards(FirebaseAuthGuard)
   async findOne(@Param() id: string) {
     return this.usersService.findUserProfileByUserId(+id);
   }
 
   @Delete(':id')
+  @UseGuards(FirebaseAuthGuard)
   async remove(@Param() id: string) {
     return this.usersService.deleteUser(+id);
   }
 
-  @Post('update/:id')
+  @Patch('update/:id')
+  @UseGuards(FirebaseAuthGuard)
   async update(@Param() id: string, @Body() updateUserDto: RegisterUserDto) {
     return this.usersService.updateUser(+id, updateUserDto);
   }
