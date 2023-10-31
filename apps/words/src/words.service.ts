bc7f75d6cd9881d538f4bbbcd21749fd3e5cf83f
Xch4rt <pab203.guti@gmail.com>
2023-10-31 03:47:18 -0600

    edit words service

--- apps/words/src/words.service.ts
+++ apps/words/src/words.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { DatabaseService } from 'libs/database/prisma/database.service';
+import { CustomException } from 'libs/common/exceptions/custom-exception';
 
 
 @Injectable()
